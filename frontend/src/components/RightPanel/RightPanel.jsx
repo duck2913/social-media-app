288f7a55d2b7a61603463311043a846794fbea5f
duck2913 <minhducdl1010@gmail.com>
2022-10-09 10:40:10 +0700

    right panel ui

--- frontend/src/components/RightPanel/RightPanel.jsx
+++ frontend/src/components/RightPanel/RightPanel.jsx
@@ -1,7 +1,31 @@
 import React from "react"
 
+const trends = [
+	{
+		name: "minion",
+		num: 16,
+	},
+	{ name: "avengers", num: 98 },
+	{ name: "birthday", num: 12 },
+	{ name: "avatar", num: 28 },
+]
+
 const RightPanel = () => {
-	return <div>RightPanel</div>
+	return (
+		<div className="h-screen p-4">
+			<div className="bg-white rounded-xl p-10">
+				<h1 className="font-semibold text-xl mb-5 text-blue-500">Trends for you</h1>
+				<div className="flex flex-col gap-4">
+					{trends.map((trend) => (
+						<div>
+							<p className="font-semibold">#{trend.name}</p>
+							<p className="text-sm">{trend.num}k shares</p>
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default RightPanel
